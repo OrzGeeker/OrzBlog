a33f0399751e6247cff2329fb42756503c539f13
wangzhizhou <824219521@qq.com>
2018-07-01 10:48:58 +0800

    add the error handler with email

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +17,55 @@ migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+
+    # Error send email
+    if app.config['MAIL_SERVER']:
+        auth=None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure=()
+
+        host=(app.config['MAIL_SERVER'],app.config['MAIL_PORT'])
+        fromaddr=app.config['MAIL_USERNAME']
+        toaddrs=app.config['ADMINS']
+        title='Microblog Failure'
+        credentials=auth
+
+        print(host)
+        print(fromaddr)
+        print(toaddrs)
+        print(title)
+        print(credentials)
+        print(secure)
+
+        mail_handler=SMTPHandler(
+            mailhost=host,
+            fromaddr=fromaddr,
+            toaddrs=toaddrs,
+            subject=title,
+            credentials=auth,
+            secure=secure,
+            timeout=5.0
+            )
+
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+    # Error log into file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler=RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
+
 
--- config.py
+++ config.py
@@ -9,4 +9,11 @@ class Config(object):
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER=os.environ.get('MAIL_SERVER') or 'smtp.qq.com'
+    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS') or 1
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME') or '824219521@qq.com'
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD') or 'ifjjerlzkmjcbbbc'
+    ADMINS=['824219521@qq.com']
+
     
