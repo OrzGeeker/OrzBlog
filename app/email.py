0752ebbea10e6d764e6f2d2e37be26de1faf2bc7
wangzhizhou <824219521@qq.com>
2018-07-08 00:00:36 +0800

    test

--- app/email.py
+++ app/email.py
@@ -2,10 +2,11 @@ from flask_mail import Message
 from app import mail, app
 from flask import render_template
 from threading import Thread
+from flask_babel import _
 
 def send_password_reset_mail(user):
     token = user.get_reset_password_token()
-    send_mail('[Microblog] Reset Your Password',
+    send_mail(_('[Microblog] Reset Your Password'),
               sender = app.config['ADMINS'][0],
               recipients= [user.email],
               text_body = render_template('email/reset_password.txt', user=user, token=token),
