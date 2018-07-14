8d29b266988cce0b7a13c6d468aa6f6d851d8f31
wangzhizhou <824219521@qq.com>
2018-07-15 00:08:52 +0800

    db upgrade

--- microblog.py
+++ microblog.py
@@ -1,11 +1,16 @@
 from app.models import User, Post
 from app import cli, create_app,db
+from app.models import Notification, Message
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db':db,'User':User,'Post':Post}
+    return {'db':db,
+            'User':User,
+            'Post':Post,
+            'Message':Message,
+            'Notification':Notification}
 
 
